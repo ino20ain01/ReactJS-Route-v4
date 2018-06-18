d29e15f66ccca5f16f13a24f6cc753ed3960542f
ChungNQ <chungnguyenquan@admicro.vn>
2018-06-18 11:21:22 +0700

    integrated react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    {/* Menu */}
+                    <nav className="navbar navbar-inverse">
+                        <div className="container-fluid">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a>Home</a></li>
+                                <li><a>About</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    {/* Content */}
+                    <Route exact path="/" component={ Home } />
+                    <Route path="/about" component={ About } />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
