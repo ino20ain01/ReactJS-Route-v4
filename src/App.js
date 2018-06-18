da8d73e7cf4bd0d76e436d2a3a6d6b0503f3f928
ChungNQ <chungnguyenquan@admicro.vn>
2018-06-18 11:53:34 +0700

    Link and NavLink

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
     render() {
@@ -13,14 +14,35 @@ class App extends Component {
                     <nav className="navbar navbar-inverse">
                         <div className="container-fluid">
                             <ul className="nav navbar-nav">
-                                <li className="active"><a>Home</a></li>
-                                <li><a>About</a></li>
+                                <li>
+                                    <NavLink
+                                        activeClassName="active"
+                                        exact
+                                        to="/"
+                                        className="my-link"
+                                    >Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        activeClassName="active"
+                                        to="/about"
+                                        className="my-link"
+                                    >About</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        activeClassName="active"
+                                        to="/contact"
+                                        className="my-link"
+                                    >Contact</NavLink>
+                                </li>
                             </ul>
                         </div>
                     </nav>
                     {/* Content */}
                     <Route exact path="/" component={ Home } />
                     <Route path="/about" component={ About } />
+                    <Route path="/contact" component={ Contact } />
                 </div>
             </Router>
         );
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                Đây là Contact
+            </div>
+        );
+    }
+}
+
+export default Contact;
