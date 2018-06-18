9a7b268bfa52020b6fabeab7ea9beb33ace0e477
ChungNQ <chungnguyenquan@admicro.vn>
2018-06-18 16:47:32 +0700

    404 - split menus and routes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
 import React, {Component} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-
-const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
-    return (
-        <Route
-            path={ to }
-            exact={ activeOnlyWhenExact }
-            children={({ match }) => {
-                let active = match ? 'active abc' : '';
-                return (
-                    <li className={active}>
-                        <Link
-                            to={ to }
-                            className="my-link"
-                        >{label}</Link>
-                    </li>
-                );
-            }}
-        />
-    );
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu from './components/Menu';
+import routes from './routes';
 
 class App extends Component {
+
+    showContentMenus = routes => {
+        let result = [];
+        if (routes.length) {
+            result = routes.map((route, index) => {
+               return (
+                   <Route
+                   key={ index }
+                   path={ route.path }
+                   exact={ route.exact }
+                   component={ route.main }
+                   />
+               );
+            });
+        }
+        return result;
+    }
 
     render() {
         return (
             <Router>
                 <div className="App">
                     {/* Menu */}
-                    <nav className="navbar navbar-inverse">
-                        <div className="container-fluid">
-                            <ul className="nav navbar-nav">
-                                <MenuLink label="Home" to="/" activeOnlyWhenExact={ true } />
-                                <MenuLink label="About" to="/about" activeOnlyWhenExact={ false } />
-                                <MenuLink label="Contact" to="/contact" activeOnlyWhenExact={ false } />
-                            </ul>
-                        </div>
-                    </nav>
+                    <Menu />
                     {/* Content */}
-                    <Route exact path="/" component={ Home } />
-                    <Route path="/about" component={ About } />
-                    <Route path="/contact" component={ Contact } />
+                    <Switch>
+                        { this.showContentMenus(routes) }
+                    </Switch>
                 </div>
             </Router>
         );
---
+++ src/components/Menu.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const menus = [
+    {
+        name: 'Home',
+        to: '/',
+        exact: true
+    },
+    {
+        name: 'About',
+        to: '/about',
+        exact: false
+    },
+    {
+        name: 'Contact',
+        to: '/contact',
+        exact: false
+    }
+];
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+    return (
+        <Route
+            path={ to }
+            exact={ activeOnlyWhenExact }
+            children={({ match }) => {
+                let active = match ? 'active unknown' : '';
+                return (
+                    <li className={`my-li ${active}`}>
+                        <Link
+                            to={ to }
+                            className="my-link"
+                        >{label}</Link>
+                    </li>
+                );
+            }}
+        />
+    );
+}
+
+class Menu extends Component {
+
+    showMenus = (menus) => {
+        let result = [];
+        if (menus.length) {
+            result = menus.map((menu, index) => {
+                return (
+                    <MenuLink
+                        key={ index }
+                        label={ menu.name }
+                        to={ menu.to }
+                        activeOnlyWhenExact={ menu.exact }
+                    />
+                );
+            });
+        }
+        return result;
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <ul className="nav navbar-nav">
+                        { this.showMenus(menus) }
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Menu;
