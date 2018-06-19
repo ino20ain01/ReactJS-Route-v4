e8f1561678f580780239aa76fefbd9ad0c53f21f
ChungNQ <chungnguyenquan@admicro.vn>
2018-06-19 08:22:32 +0700

    Prompt in react route

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isChecked: false
+        }
+    }
+
+    onClick = isChecked => {
+        this.setState({
+            isChecked
+        });
+    }
+
     render() {
+
+        let { isChecked } = this.state;
+
         return (
             <div>
-                Đây là Contact
+                <p>Đây là Contact</p>
+                <button type="button" className="btn btn-primary" onClick={ () => { this.onClick(false) } } >Cho phép !</button> &nbsp;
+                <button type="button" className="btn btn-danger" onClick={ () => { this.onClick(true) } }>Không cho phép !</button>
+                <Prompt
+                    when={ isChecked }
+                    message={ location => (`Bạn chắc chắn muốn đi đến ${location.pathname}`) }
+                />
             </div>
         );
     }
